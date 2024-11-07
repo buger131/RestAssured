15ce03686e66d5925fc5b14c7c5ae5f0ed516165
Andrew <mus.drake@gmail.com>
2024-11-07 19:19:59 +0300

    Добавил тестовую аннотацию для демонстрации

--- src/test/java/api/project_name/TestProject.java
+++ src/test/java/api/project_name/TestProject.java
@@ -1,6 +1,7 @@
 package api.project_name;
 
 import api.BaseTest;
+import api.annotations.User;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -8,6 +9,7 @@ public class TestProject extends BaseTest {
 
     @Test
     @DisplayName("test method")
+    @User(login = "new_login", password = "new_password")
     void testExample() {
         extractResponse("url",
                 "src/test/java/api/project_name/schema/test_schema.json",
